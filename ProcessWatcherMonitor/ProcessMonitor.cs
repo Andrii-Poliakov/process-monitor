5bee5b0e9aa3f8849715b34b357780043d6a658b
Andrii-Poliakov <156352763+Andrii-Poliakov@users.noreply.github.com>
2025-10-08 15:22:08 -0600

    1

--- ProcessWatcherMonitor/ProcessMonitor.cs
+++ ProcessWatcherMonitor/ProcessMonitor.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace ProcessWatcherMonitor
+namespace ProcessWatcherMonitor 
 {
 
     public class ProcessMonitor
